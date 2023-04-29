7db180b4ed4b9dac3e89ea4f853c11ab53320f6a
sjcho-90 <sinrakr1@gmail.com>
2023-04-29 23:29:54 +0900

    - 구현 개념 + 10장 문풀 추가

---
+++ 4장.구현/10.자물쇠와열쇠.py
@@ -0,0 +1,45 @@
+key = [[0, 0, 0], [1, 0, 0], [0, 1, 1]]
+lock = [[1, 1, 1], [1, 1, 0], [1, 0, 1]]
+
+
+def rotate(key):
+    new_key = []
+    for i in zip(*key):
+        temp_list = list(i)
+        temp_list.reverse()
+        new_key.append(temp_list)
+    return new_key
+
+def check(new_lock):
+    lock_length = len(new_lock) // 3
+    for i in range(lock_length, lock_length*2):
+        for j in range(lock_length, lock_length*2):
+            if new_lock[i][j] != 1:
+                return False
+    return True
+
+def solution(key, lock):
+    n = len(lock)
+    m = len(key)
+    new_lock = [[0]* (n*3) for _ in range(n*3)]
+    for i in range(n):
+        for j in range(n):
+            new_lock[i+n][j+n] = lock[i][j]
+
+    for rotation in range(4):
+        key = rotate(key)
+        for x in range(n * 2):
+            for y in range(n * 2):
+                for i in range(m):
+                    for j in range(m):
+                        new_lock[x + i][y + j] += key[i][j]
+                    if check(new_lock):
+                        return True
+                for i in range(m):
+                    for j in range(m):
+                        new_lock[x + i][y + j] -= key[i][j]
+    return False
+
+#
+#
+print(solution(key, lock))
