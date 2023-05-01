4a46479d6a39544cf6ad1aaaec8d0c9bb2e28673
blessedby-clt <sinrakr1@gmail.com>
2023-05-01 17:56:45 +0900

    - DFS-BFS 개념 추가 - 18번 문풀 추가

---
+++ 5장.DFS-BFS/18.괄호변환.py
@@ -0,0 +1,41 @@
+p = "(()())()"
+
+from collections import Counter
+def balanced_index(p):
+    for idx in range(len(p)):
+        counter = Counter(p[:idx+1])
+        if counter["("] == counter[")"]:
+            return idx+1
+def check_proper(p):
+    before_value = ''
+    after_value = p
+    while before_value != after_value:
+        before_value = after_value
+        after_value = after_value.replace("()", "")
+    if len(after_value) > 0:
+        return False
+    else:
+        return True
+
+def solution(p):
+    answer = ""
+    if p == "":
+        return answer
+
+    index = balanced_index(p)
+    u = p[:index]
+    v = p[index:]
+
+    if check_proper(u):
+        answer = u + solution(v)
+    else:
+        answer = "("
+        answer += solution(v)
+        answer += ")"
+        u = u[1:-1]
+        u = u[::-1]
+        answer += u
+    return answer
+
+
+print(solution(p))
