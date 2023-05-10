f592c16d2500acfc5bbfcf70a8e74973f0e5c306
blessedby-clt <sinrakr1@gmail.com>
2023-05-11 08:52:09 +0900

    - 백준 안전영역 문제 풀이

--- 5장.DFS-BFS/(백준)02.안전영역.py
+++ 5장.DFS-BFS/(백준)02.안전영역.py
@@ -1,30 +1,47 @@
-n = int(input())
-visit_graph = [[False]*n for i in range(n)]
+import sys
+sys.setrecursionlimit(100000)
+
+n = int(sys.stdin.readline())
+# visit_graph = [[False]*n for i in range(n)]
+
 map_graph = []
 for i in range(n):
-    map_graph.append(list(map(int, input().split())))
+    map_graph.append(list(map(int, sys.stdin.readline().split())))
 
 dx = [1, -1, 0, 0]
 dy = [0, 0, 1, -1]
-def dfs(r, c, n):
+#
+max_height = 0
+for i in map_graph:
+    temp_max = max(i)
+    if max_height < temp_max:
+        max_height = temp_max
+
+def dfs(r, c, h):
     visit_graph[r][c] = True
-    if map_graph[r][c] > n :
+    if map_graph[r][c] > h:
         safe_ground.append((r, c))
         for i in range(4):
             nx = r + dx[i]
             ny = c + dy[i]
             if nx >= 0 and ny >= 0 and nx < n and ny < n:
                 if visit_graph[nx][ny] == False:
-                    dfs(nx, ny, n)
+                    dfs(nx, ny, h)
 
-result = 0
-safe_ground = []
-for r in range(n):
-    for c in range(n):
-        if visit_graph[r][c] == False:
-            dfs(r, c, n)
-            if len(safe_ground) > 0:
-                result += 1
-                safe_ground = []
+answer = 1
+for height in range(1, max_height+1):
+    result = 0
+    safe_ground = []
+    visit_graph = [[False] * n for i in range(n)]
+    for r in range(n):
+        for c in range(n):
+            if visit_graph[r][c] == False:
+                dfs(r, c, height)
+                if len(safe_ground) > 0:
+                    result += 1
+                    safe_ground = []
+    # print(height, ":", result)
+    if answer < result:
+        answer = result
 
-print(result)
+print(answer)
